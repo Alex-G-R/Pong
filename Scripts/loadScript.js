a46c859ae3528435e1bc1fb98f8f120385d2178b
Alex-G-R <alex.grabski555@gmail.com>
2023-08-16 02:08:47 +0200

    Improved working of back buttons

--- Scripts/loadScript.js
+++ Scripts/loadScript.js
@@ -48,22 +48,48 @@ modeButtons.forEach((button) =>{
 const pvpMode = document.getElementById("mode-pvp");
 const pvcMode = document.getElementById("mode-pvc");
 const campainMode = document.getElementById("mode-campain");
+const campainMenu = document.getElementById("campain-menu");
 const difficultyChoice = document.getElementById("choose-difficulty");
 
 const pvcButton = document.getElementById("pvc-button");
+const campainButton = document.getElementById("campain-button");
+
+const pvcBack = document.getElementById("back-button-pvc");
+
+const campBack = document.getElementById("back-button-campain");
 
 pvcButton.addEventListener("click", e => {
     pvpMode.style.display = "none";
     pvcMode.style.display = "none";
     campainMode.style.display = "none";
     difficultyChoice.style.display = "block";
-})
+    pvcBack.style.display = "block";
+});
 
-// Back button
-const backButton = document.getElementById("back-button");
+campainButton.addEventListener("click", e => {
+    pvpMode.style.display = "none";
+    pvcMode.style.display = "none";
+    campainMode.style.display = "none";
+    difficultyChoice.style.display = "none";
+    campBack.style.display = "block";
 
-backButton.addEventListener("click", e => {
+    campainMenu.style.display = "block";
+});
+
+pvcBack.addEventListener("click", e =>{
     pvpMode.style.display = "block";
     pvcMode.style.display = "block";
+    campainMode.style.display = "block";
     difficultyChoice.style.display = "none";
-});
+    pvcBack.style.display = "none";
+    campainMenu.style.display = "none";
+})
+
+campBack.addEventListener("click", e =>{
+    pvpMode.style.display = "block";
+    pvcMode.style.display = "block";
+    campainMode.style.display = "block";
+    difficultyChoice.style.display = "none";
+    pvcBack.style.display = "none";
+    campainMenu.style.display = "none";
+})
