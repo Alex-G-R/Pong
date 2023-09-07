f2141d7a325d2a9467e9238a379b16dd262187f8
Alex-G-R <alex.grabski555@gmail.com>
2023-09-07 17:34:49 +0200

    Fixing movment issue

--- Scripts/startCustomGame.js
+++ Scripts/startCustomGame.js
@@ -275,7 +275,7 @@ export function startCustomGame(
         if(!mirrorMode){
             if (event.code == "ArrowUp" && playerY > 0) {
                 opponentVelocity = -opponentSpeed;
-            } else if (event.code == "ArrowDown" && playerY + playerHeight < board.height) {
+            } else if (event.code == "ArrowDown" && opponentY + opponentHeight < board.height) {
                 opponentVelocity = opponentSpeed;
             } else if (event.code == "KeyW" && playerY > 0) {
                 velocityY = -playerSpeed;
